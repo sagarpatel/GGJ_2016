425ec31f4a1f4fb4eee4a9a5e7e6c8b19ad2f902
Sagar Patel <17.sagar.patel@gmail.com>
2016-01-31 16:07:38 +0900

    Implemented music, looks like giuls theme is public domain (on archive.org)

--- GGJ_2016/Assets/Scripts/AudioDirector.cs
+++ GGJ_2016/Assets/Scripts/AudioDirector.cs
@@ -6,6 +6,11 @@ public class AudioDirector : MonoBehaviour
     public AudioClip[] m_pingpongSFXArray;
     public AudioClip m_pop1;
     
+    void Start()
+    {
+        FindObjectOfType<Camera>().GetComponent<AudioSource>().PlayDelayed(0.710f);
+    }  
+    
     public AudioClip RandomPPSFX()
     {
         int randomIndex = Random.Range(0, m_pingpongSFXArray.Length);
