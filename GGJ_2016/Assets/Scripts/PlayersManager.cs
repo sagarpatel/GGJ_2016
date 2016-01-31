af30c2e8769fe2bfcc5ad608a502940a02f12469
Sagar Patel <17.sagar.patel@gmail.com>
2016-01-31 11:27:49 +0900

    Implemented basic player spawning working

--- GGJ_2016/Assets/Scripts/PlayersManager.cs
+++ GGJ_2016/Assets/Scripts/PlayersManager.cs
@@ -4,6 +4,33 @@ using System.Collections;
 public class PlayersManager : MonoBehaviour
 {
 
+    public GameObject m_playerPrefab;
+
+    public Transform m_playerSpawnPos_1;
+    public Transform m_playerSpawnPos_2;
+
+    public Material m_playerMaterial_1;
+    public Material m_playerMaterial_2;
+
+    GameObject m_player_1;
+    GameObject m_player_2;
+
+    int m_roundCount = 0;
+
+    void Start()
+    {
+        m_player_1 = (GameObject)Instantiate(m_playerPrefab, m_playerSpawnPos_1.position, Quaternion.identity);
+        m_player_1.GetComponent<PlayerMaterialSet>().m_playerMaterial = m_playerMaterial_1;
+        m_player_1.GetComponentInChildren<PlayerMove>().m_playerIndex = 0;
+        m_player_1.name = "Player_1";
+
+
+        m_player_2 = (GameObject)Instantiate(m_playerPrefab, m_playerSpawnPos_2.position, Quaternion.identity);
+        m_player_2.GetComponent<PlayerMaterialSet>().m_playerMaterial = m_playerMaterial_2;
+        m_player_2.GetComponentInChildren<PlayerMove>().m_playerIndex = 1;
+        m_player_2.name = "Player_2";
+    }
+
     public void KillPlayer(Transform playerTransform)
     {
         playerTransform.GetComponent<PlayerExplode>().Explode();
