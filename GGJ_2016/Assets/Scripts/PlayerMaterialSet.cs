384db71415b540214fd34724ad81f01c103a6178
Sagar Patel <17.sagar.patel@gmail.com>
2016-01-31 12:57:11 +0900

    Implemented dead materials system

--- GGJ_2016/Assets/Scripts/PlayerMaterialSet.cs
+++ GGJ_2016/Assets/Scripts/PlayerMaterialSet.cs
@@ -3,19 +3,32 @@ using System.Collections;
 
 public class PlayerMaterialSet : MonoBehaviour
 {
-    public Material m_playerMaterial;
+    public Material m_aliveMaterial;
+    public Material m_deadMaterial;
 
     void Start()
+    {
+        SetPlayerMaterial(m_aliveMaterial);
+    }
+
+    public void SetDeadMaterial()
+    {
+        SetPlayerMaterial(m_deadMaterial);
+    }
+
+    void SetPlayerMaterial(Material playerMaterial)
     {
         Renderer[] renderersArray = GetComponentsInChildren<Renderer>();
         for(int i = 0; i < renderersArray.Length; i++)
         {
-            renderersArray[i].sharedMaterial = m_playerMaterial;
+            renderersArray[i].sharedMaterial = playerMaterial;
         }
 
         Light ballLight = GetComponentInChildren<Light>();
-        ballLight.color = m_playerMaterial.color;
+        if(ballLight != null)
+            ballLight.color = playerMaterial.color;
     }
+
 
 
 }
--- GGJ_2016/Assets/Scripts/PlayersManager.cs
+++ GGJ_2016/Assets/Scripts/PlayersManager.cs
@@ -12,6 +12,10 @@ public class PlayersManager : MonoBehaviour
     public Material m_playerMaterial_1;
     public Material m_playerMaterial_2;
 
+    public Material m_playerMaterial_Dead_1;
+    public Material m_playerMaterial_Dead_2;
+
+
     GameObject m_player_1;
     GameObject m_player_2;
 
@@ -26,13 +30,15 @@ public class PlayersManager : MonoBehaviour
     void SpawnPlayers()
     {
         m_player_1 = (GameObject)Instantiate(m_playerPrefab, m_playerSpawnPos_1.position, Quaternion.identity);
-        m_player_1.GetComponent<PlayerMaterialSet>().m_playerMaterial = m_playerMaterial_1;
+        m_player_1.GetComponent<PlayerMaterialSet>().m_aliveMaterial = m_playerMaterial_1;
+        m_player_1.GetComponent<PlayerMaterialSet>().m_deadMaterial = m_playerMaterial_Dead_1;
         m_player_1.GetComponentInChildren<PlayerMove>().m_playerIndex = 0;
         m_player_1.name = "Player_1";
 
 
         m_player_2 = (GameObject)Instantiate(m_playerPrefab, m_playerSpawnPos_2.position, Quaternion.identity);
-        m_player_2.GetComponent<PlayerMaterialSet>().m_playerMaterial = m_playerMaterial_2;
+        m_player_2.GetComponent<PlayerMaterialSet>().m_aliveMaterial = m_playerMaterial_2;
+        m_player_2.GetComponent<PlayerMaterialSet>().m_deadMaterial = m_playerMaterial_Dead_2;
         m_player_2.GetComponentInChildren<PlayerMove>().m_playerIndex = 1;
         m_player_2.name = "Player_2";
 
@@ -49,10 +55,21 @@ public class PlayersManager : MonoBehaviour
 
     IEnumerator KillRespawnSequence(Transform firstKill)
     {
+        firstKill.GetComponent<PlayerMaterialSet>().SetDeadMaterial();
+
+        yield return new WaitForSeconds(0.25f);
+
+        // TODO:  do slomo and cam shake here
 
         firstKill.GetComponent<PlayerExplode>().Explode();
 
-        yield return new WaitForSeconds(0.750f);
+        yield return new WaitForSeconds(1.0f);
+
+        m_player_1.GetComponent<PlayerMaterialSet>().SetDeadMaterial();
+        m_player_2.GetComponent<PlayerMaterialSet>().SetDeadMaterial();
+
+
+        yield return new WaitForSeconds(0.25f);
 
         // kill both again because i can't be bothered wit id system
         m_player_1.GetComponent<PlayerExplode>().Explode();
