11aeffd7891743f1861bf3edce83f13b22155481
Sagar Patel <17.sagar.patel@gmail.com>
2016-01-31 15:39:18 +0900

    Implemented ping pong sfx

---
+++ GGJ_2016/Assets/Scripts/AudioDirector.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioDirector : MonoBehaviour
+{
+    public AudioClip[] m_pingpongSFXArray;
+
+    
+    public AudioClip RandomPPSFX()
+    {
+        int randomIndex = Random.Range(0, m_pingpongSFXArray.Length);
+        return m_pingpongSFXArray[randomIndex];
+    }
+
+}
--- GGJ_2016/Assets/Scripts/PlayerBall.cs
+++ GGJ_2016/Assets/Scripts/PlayerBall.cs
@@ -4,14 +4,20 @@ using System.Collections;
 public class PlayerBall : MonoBehaviour
 {
     PlayersManager m_playersManager;
+    AudioSource m_audioSource;
+    AudioDirector m_audioDirector;
 
     void Start()
     {
         m_playersManager = FindObjectOfType<PlayersManager>();
+        m_audioSource = GetComponent<AudioSource>();
+        m_audioDirector = FindObjectOfType<AudioDirector>();
     }
 
     void OnCollisionEnter(Collision collision)
     {
+        m_audioSource.PlayOneShot(m_audioDirector.RandomPPSFX());
+
         if (collision.transform.CompareTag("Floor"))
         {            
             m_playersManager.KillPlayer(transform.root);
